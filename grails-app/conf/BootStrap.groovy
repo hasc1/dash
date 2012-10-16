4f3bb1314c27c54ce15a7d91f1847856e80a4d05
Gunjan Sharma <gunjans.sharma@gmail.com>
2012-10-15 22:15:24 -0700

    Added bootstrap data for user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,11 @@
+import grails.util.GrailsUtil
+
+
 class BootStrap {
 
     def init = { servletContext ->
-environments {
-    development {
+		switch(GrailsUtil.environment){
+			case "development":
     	//if (!hascstudy.Hospital.count()) 
     	//{
     	def hospital = new hascstudy.Hospital(hospitalName:"Hospital1")
@@ -47,11 +50,33 @@ environments {
 		reportCategory = new hascstudy.ReportCategory(name:"Category 3")
 		reportCategory.save()
 
+		def admin = new hascstudy.User(login:"admin",
+			password:"password",
+			name:"Administrator",
+			role:"admin")
+			admin.save()
 		
+		def jdoe = new hascstudy.User(login:"jdoe",
+				   password:"password",
+				   name:"John Doe",
+				   role:"author")
+				jdoe.save()
+		//snip...
+		
+		def jsmith = new hascstudy.User(login:"jsmith",
+				   password:"wordpass",
+				   name:"Jane Smith",
+				   role:"author")
+		jsmith.save()
+		//snip...
+		
+		break
+		
+		case "production":
+		break
 		
 		
 		        //}
-	}
 	}
 	}
     def destroy = {
